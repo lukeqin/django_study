334431dae67a17860c6dbd6790a11586ad7ac24d
lukeql <lukeqinlu@yeah.com>
2020-03-26 22:56:06 +0800

    Change git config local

--- mysite/polls/test.py
+++ mysite/polls/test.py
@@ -135,3 +135,5 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+        
